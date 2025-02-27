15f583cde9d68d81e7a4c16429d62b5bedc5233a
spshrishail <161191083+spshrishail@users.noreply.github.com>
2025-02-27 19:14:55 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://chatbackend-tau.vercel.app',
+        changeOrigin: true,
+        secure: true,
+        
+      },
+    },
+  },
 })
